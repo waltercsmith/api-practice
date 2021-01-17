2c1d9bd78eaa3ef3f771dedd3dcd974eaee789ca
Walter Smith <walter.c.smith01@gmail.com>
2021-01-16 21:06:46 -0600

    Used a fetch api to illustrate a promise

--- js/api-practice.js
+++ js/api-practice.js
@@ -4,7 +4,11 @@
 //     .then(response => (response))
 //     .catch(error => (error));
 
-const myPromise = fetch('https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49');
-myPromise.then(response => console.log(response) );
-myPromise.then(error => console.log(error));
+// const myPromise = fetch('https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49');
+// myPromise.then(response => console.log(response) );
+// myPromise.then(error => console.log(error));
 
+fetch('https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49')
+.then(response => {response.json() // when I justt left with the .json() then nothing was logged in the console
+    console.log(response);
+})
